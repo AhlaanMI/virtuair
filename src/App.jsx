c2ff54e9bacaf7438b98723cec05677398d29e6d
ahlaan <mohamedahlan58@gmail.com>
2024-10-24 17:58:15 +0530

    Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection.jsx'
 import FeatureSection from './components/FeatureSection.jsx'
+import Workflow from './components/Workflow.jsx'
+import Pricing from './components/Pricing.jsx'
+import Testimonials from './components/Testimonials.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
@@ -10,7 +14,14 @@ const App = () => {
     <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection/>
+        <Workflow/>
+        <Pricing/>
+        <Testimonials/>
+        <Footer/>
+        
+
       </div>
+      
     </>
   )
 }
