29b665ef47cf43698883f0c7eb4ec5aea0ab0f73
ahlaan <mohamedahlan58@gmail.com>
2024-10-24 13:22:09 +0530

    fin nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,6 +33,7 @@ const Navbar = () => {
               href="#"
               className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
             >
+              Create an account
             </a>
           </div>
           <div className="lg:hidden md:flex flex-col justify-end">
@@ -52,14 +53,13 @@ const Navbar = () => {
             </ul>
             <div className="flex space-x-6">
               <a href="#" className="py-2 px-3 border rounded-md">
-                Sign 
+                Sign In
               </a>
               <a
                 href="#"
                 className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
               >
                 Create an account
-                hello
               </a>
             </div>
           </div>
